7135122d4dbf0b3ea7680c982048f438492b83ed
Jiubei0408 <757795210@qq.com>
2020-11-24 11:31:30 +0800

    添加两个钩子，修改轮盘赌比例算法

--- GA/__init__.py
+++ GA/__init__.py
@@ -38,7 +38,7 @@ class Gene(ABC):
         pass
 
 
-class GA():
+class GA:
     def __init__(self, gene_generator, **kwargs):
         self.genes = []
         self.gene_generator = gene_generator  # 随机基因生成器
@@ -55,13 +55,14 @@ class GA():
     def run(self):
         self.generate_genes()
         for i in range(1, self.generations + 1):
+            self.before_one_generation(i, self.genes)
             self.one_generation()
-            print(f'generation: {i}, x={self.genes[0].decode()}, y={self.genes[0].fitness}')
+            self.after_one_generation(i, self.genes)
 
     def one_generation(self):
         mi = min([i.fitness for i in self.genes])
         mx = max([i.fitness for i in self.genes])
-        rates = [(gene.fitness - mi + 1) / (mx - mi + 1) for gene in self.genes]
+        rates = [(gene.fitness - mi + 1) ** 2 / (mx - mi + 1) for gene in self.genes]
         children = []
         for i in range(math.floor(len(self.genes) * self.cross_rate)):
             a, b = random.choices(self.genes, weights=rates, k=2)
@@ -71,3 +72,9 @@ class GA():
         self.genes += children
         self.genes.sort(key=lambda x: x.fitness, reverse=True)
         self.genes = self.genes[:self.safe_num]
+
+    def before_one_generation(self, generation, genes):
+        pass
+
+    def after_one_generation(self, generation, genes):
+        pass
